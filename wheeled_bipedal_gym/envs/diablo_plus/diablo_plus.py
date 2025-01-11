seem to change the theta-calculation wrong, return to the old type

wheeled_bipedal_gym/envs/diablo_plus/diablo_plus.py
@@ -152,15 +152,15 @@ class WheeledBipedal(BaseTask):
         # change from original for the joint tf is different！
         self.theta1 = torch.cat(
             (
-                self.dof_pos[:, 0].unsqueeze(1) - 0.7,
-                self.dof_pos[:, 3].unsqueeze(1) - 0.7,
+                self.dof_pos[:, 0].unsqueeze(1) + self.pi - 0.13433,
+                self.dof_pos[:, 3].unsqueeze(1) + self.pi - 0.13433,
             ),
             dim=1,
         )
         self.theta2 = torch.cat(
             (
-                (self.dof_pos[:, 1] + 1.6).unsqueeze(1),
-                (self.dof_pos[:, 4] + 1.6).unsqueeze(1),
+                (self.dof_pos[:, 1] - self.pi + 0.26866).unsqueeze(1),
+                (self.dof_pos[:, 4] - self.pi + 0.26866).unsqueeze(1),
             ),
             dim=1,
         )
@@ -227,7 +227,7 @@ class WheeledBipedal(BaseTask):
         l1 = self.cfg.asset.l1
         l2 = self.cfg.asset.l2
         theta1 = self.theta1
-        theta2 = self.theta2
+        theta2 = self.theta2 - 0.349066
         x = l1 * torch.cos(theta1) + l2 * torch.cos(theta1 + theta2 - 0.349066)
         y = l1 * torch.sin(theta1) + l2 * torch.sin(theta1 + theta2 - 0.349066)
 

wheeled_bipedal_gym/scripts/play.py
@@ -104,7 +104,7 @@ def play(args):
     img_idx = 0
     latent = None
 
-    CoM_offset_compensate = True
+    CoM_offset_compensate = False
     vel_err_intergral = torch.zeros(env.num_envs, device=env.device)
     vel_cmd = torch.zeros(env.num_envs, device=env.device)
 
@@ -115,7 +115,7 @@ def play(args):
             actions = policy(obs.detach())
 
         env.commands[:, 0] = 0.0
-        env.commands[:, 2] = 0.25 + 0.07 * np.sin(i * 0.01)
+        env.commands[:, 2] = 0.25  # + 0.07 * np.sin(i * 0.01)
         env.commands[:, 3] = 0
 
         if CoM_offset_compensate: